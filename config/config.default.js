ffb61eeac676c0a19d27dba40e059be831218976
nkepling <nathankep@gmail.com>
2024-12-03 23:33:05 +0000

    changes

--- config/config.default.js
+++ config/config.default.js
@@ -3,6 +3,9 @@
 var config = require('./config.webgme'),
     validateConfig = require('webgme/config/validator');
 
+
+// Add requirejs paths
+config.requirejsPaths.plotly = './node_modules/plotly.js/dist';
 // Add/overwrite any additional settings here
 // config.server.port = 8080;
 // config.mongo.uri = 'mongodb://127.0.0.1:27017/webgme_my_app';
